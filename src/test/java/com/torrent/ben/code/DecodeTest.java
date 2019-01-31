667ac347d330d74227b4eacfea64fb59b53e47c3
Juan Pablo <juanpablolvl99@gmail.com>
2019-01-31 14:09:08 -0300

    adding tests for decode

---
+++ src/test/java/com/torrent/ben/code/DecodeTest.java
@@ -0,0 +1,88 @@
+package com.torrent.ben.code;
+
+import com.torrent.ben.code.de.BDecoder;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DecodeTest {
+
+    private BDecoder decoder;
+    {
+        this.decoder = new BDecoder();
+    }
+
+    // number tests
+    @Test
+    public void decodeInt() {
+        Object decoded = this.decoder.decode("i-3e");
+        Assert.assertEquals(decoded.toString(), "-3");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void decodeMalformedInt() {
+        this.decoder.decode("i4f5e");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void decodeEmptyInt() {
+        this.decoder.decode("ie");
+    }
+
+    // string tests
+    @Test
+    public void decodeString() {
+        Object decoded = this.decoder.decode("5:aberf");
+        Assert.assertEquals(decoded.toString(), "aberf");
+    }
+
+    @Test
+//            (expected = ArrayIndexOutOfBoundsException.class)
+    public void decodeMalformedString() {
+        // length > string.length
+        Object decoded = this.decoder.decode("6:aberf");
+    }
+
+    @Test
+    public void decodeEmptyString() {
+        Object decoded = this.decoder.decode("0:");
+        Assert.assertEquals(decoded.toString(), "");
+    }
+
+    // list tests
+    @Test
+    public void decodeList() {
+        Object decoded = this.decoder.decode("li5e3:abce");
+        Assert.assertEquals(decoded.toString(), "[5, abc]");
+    }
+
+    @Test
+    public void decodeEmptyList() {
+        Object decoded = this.decoder.decode("le");
+        Assert.assertEquals(decoded.toString(), "[]");
+    }
+
+    // dict tests
+    @Test
+    public void decodeDict() {
+        Object decoded = this.decoder.decode("d4:name6:kanedae");
+        Assert.assertEquals(decoded.toString(), "{name=kaneda}");
+    }
+
+    @Test
+    public void decodeEmptyDict() {
+        Object decoded = this.decoder.decode("de");
+        Assert.assertEquals(decoded.toString(), "{}");
+    }
+
+    // other tests
+    @Test
+    public void decodeEmptyData() {
+        Object decoded = this.decoder.decode("");
+        Assert.assertEquals(decoded, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void decodeInvalidDelimiter() {
+        this.decoder.decode("r4e");
+    }
+}
